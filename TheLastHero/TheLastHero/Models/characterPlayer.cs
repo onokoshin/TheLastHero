def630b9526b239461d9ca99d9166e996f735dd4
onokoshin <onokoshin@gmail.com>
2018-01-19 16:59:10 -0800

    First Commit

---
+++ TheLastHero/TheLastHero/Models/characterPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheLastHero.Models
+{
+    //playerCharacter inherites base attributes and methods from characterbase class
+    class playerCharacter : character
+    {
+
+        //constructor
+        public playerCharacter()
+        {
+
+        }
+
+        //increases character's level
+        public void Increaselevel()
+        {
+
+        }
+
+        public void IncreaseExperience()
+        {
+
+        }
+
+       
+    }
+}
---
+++ TheLastHero/TheLastHero/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TheLastHero.Models;
+
+namespace TheLastHero.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
