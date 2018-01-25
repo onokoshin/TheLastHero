f83cea78736c447da22d15d961914b828ac4c43b
SiyaoX <siyaoxu817@gmail.com>
2018-01-24 18:19:43 -0800

    Add CharacterPage

---
+++ TheLastHero/TheLastHero/Views/CharacterPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TheLastHero.Views
+{
+    public partial class CharacterPage : ContentPage
+    {
+        public CharacterPage()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
