6d6a3dc7817883ff95ce484fe1d551b4e1965382
SiyaoX <siyaoxu817@gmail.com>
2018-01-24 18:27:13 -0800

    Add missing img

--- TheLastHero/TheLastHero/Models/Creature/Character/Character.cs
+++ TheLastHero/TheLastHero/Models/Creature/Character/Character.cs
@@ -4,24 +4,20 @@ using System.Text;
 
 namespace TheLastHero.Models
 {
-    
+
     //playerCharacter inherites base attributes and methods from characterbase class
     class Character : creature
     {
         //contant set to the maximum level 
         public const int capLevel = 20;
-        
+
         //CHARACTER ONLY
         public int nextLevelExp { get; set; }
         public int currentExp { get; set; }
-
         public bool isCapLevel { get; set; }
 
-
-       
-
         // using enum for location 
-        public enum locations { head, body, feet, leftHand, rightHand, leftFinger, rightFinger  }
+        public enum locations { head, body, feet, leftHand, rightHand, leftFinger, rightFinger }
 
         //Key - location is from enum, Value - Item 
         public Dictionary<locations, Item> equippedItem;
@@ -31,5 +27,7 @@ namespace TheLastHero.Models
         {
             //maybe instantiate equippedItem so that characters can have weapons 
         }
+
+
     }
 }
