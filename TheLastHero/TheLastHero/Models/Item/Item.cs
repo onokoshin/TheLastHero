5523c763d07db36be0ff44c256dcc8abcac9105a
SiyaoX <siyaoxu817@gmail.com>
2018-02-08 22:56:36 -0800

    * build.props: Checked in Item code
    * Battle.cs:
    * Item.cs:
    * TheLastHero.projitems:
    * BattlePage.xaml.cs:
    * ItemManager.cs:
    * TurnManager.cs:
    * MonsterManager.cs:
    * LevelingManager.cs:
    * CharacterManager.cs:
    * TheLastHero.iOS.csproj.CoreCompileInputs.cache:

--- TheLastHero/TheLastHero/Models/Item/Item.cs
+++ TheLastHero/TheLastHero/Models/Item/Item.cs
@@ -20,7 +20,20 @@ namespace TheLastHero.Models
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+
+        // There will be 4 types of items: "Weapon", "Armor", "Ring",
+        // "Consumable"
+        public string Type { get; set; }
+
+        // There will be of items: Head, LeftHand, RightHand, Body, Leg,
+        // Foot, Left Finger, Right Finger
+        public string Location { get; set; }
+        public int Lvl { get; set; }
+        public int Def { get; set; }
+        public int Atk { get; set; }
+        public int Spd { get; set; }
+        public int Agi { get; set; }
+        public string SpecialAbility { get; set; }
+
     }
-
-
 }
--- TheLastHero/TheLastHero/Views/BattlePage.xaml.cs
+++ TheLastHero/TheLastHero/Views/BattlePage.xaml.cs
@@ -1,13 +1,54 @@
-﻿using System;
+﻿
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TheLastHero.GameEngine;
 using TheLastHero.Models;
 using Xamarin.Forms;
 
+/** This is our battle controller. all logical actions related to battle are 
+ * written here. We will use a Queue structure for turn management, the queue 
+ * takes all creatures based on their speed. Hence, the queue is in an order of 
+ * which creature goes next. The queue is declared as “nextOneQueue” in our code 
+ * below.  In order to store creatures into a queue in a proper order, we are
+ * going to sort both character list and monster list. Then we will compare head 
+ * of each list’s creature’s speed to determine which creature would be enqueued
+ * into our queue. This way, creatures with higher speed will get their turn 
+ * first. Once, a creature is dequeued, we will first check whether the 
+ * creature is alive or dead based on creature’s ‘liveStatus.’ 
+ * If the creature is dead, we skip and not enqueue the creature back to
+ * the queue. If the creature is alive, we will use turnManager to control 
+ * its actions.   
+
+Turn manager,turnManager, will control all the creatures’ battle actions: move, 
+attack, skip and etc. In every round, every creature only be allowed to control 
+action once. Character movement will be decide and input by user then
+turnManager will implement users’ turn decisions. Monster movement will be 
+decided by Game engine randomly. The turnManager will oftern update if any 
+creature is eliminated in each round.
+
+Grid Layout ,battleGrid, is our battle game board, we have a 4x6 size board, 
+each grid element is clickable. We will capture the click event and do 
+calculations and updates here. The position of monsters and characters are 
+memorized by a 2 dimensional integer array. Int [][] map, 0 indicate this
+tile is empty, if a monster or character is occupying this tile, we will 
+register the array with its ID. Later on, we can use this map to do movement 
+and attack calculation.  
+
+There will be 6 monsters in each battle. The monster will grow stronger each 
+round linearly, and our character has a cap level at 20. So eventually, all 
+the monsters will become very powerful and kill all the characters. Every 4 
+rounds, the characters will engage a boss fight, which there is only one 
+monster. The boss monster is 6 times stronger than the normal monster. The 
+BOSSROUND is a const indicate every 4 rounds there will be the boss fight.
+
+**/
 namespace TheLastHero.Views
 {
     public partial class BattlePage : ContentPage
     {
+
+        // Every four rounds, game engine will generate a new boss monster
         const int BOSSROUND = 4;
 
         // Batstle map is a grid layout  
@@ -16,7 +57,7 @@ namespace TheLastHero.Views
         //use 2d array to instantiate a battlemap 6 x 4
         int[,] battleMap = new int[6, 4];
 
-        // The data structure list contains selected six characters passed from SelectCharacterPage
+        // The data structure list contains selected six characters passed from    SelectCharacterPage
         public List<Character> characters;
 
         // The data structure list contains contains six monsters that will appear in each battle
@@ -26,25 +67,34 @@ namespace TheLastHero.Views
         // List of Items 
         public List<Item> itemPool;
 
-        // indicates which level the player is playing, and it also tells manager to handle round related 
-        // issues.
+        // indicates which level the player is playing, and it also tells manager to handle round related subjects.
         public int currentRound;
-
         // this queue tells game turn manager which creature is next to be able to move around, attack, or // equip it
         // The queue stores characters and monsters in an order of speed, in other words, they are stored in an order of which creature moves next 
         public Queue<Creature> nextOneQueue;
+        // damage calculations are done inside this helper class. It has member function such as 
+        // applyDamage(Creature attacker, Creature defender)
+        DamageCalculation damageCalculation;
+        // Turn manager will control all the creatures’ battle actions such as, move, attack, skip and etc.
+        // usage example: turnManager.move(character_1, “forward”) and
+        // turnManager.attack(character1, monster2)
+        TurnManager turnManager;
 
-
+        //Constructor 
         public BattlePage()
         {
-
             InitializeComponent();
         }
+
+
+
+        // This method will be called upon beginning of the game, character movement,         
+        //   monster movement, character die, monster die events. 
 
         public void startGame()
         {
             nextOneQueue = new Queue<Creature>();
-            // read 
+
 
             //TurnManager turnManager = new TurnManager();
 
@@ -52,22 +102,25 @@ namespace TheLastHero.Views
             // nextOneQueue = InitializeQueue(monsters, characters);
             while (true)
             {
+                // If either number of characters or number of monster equals zero, the battle is over.
                 if (characters.Count == 0)
-                    //Game over
-                    // pushAsync(Report.xaml)   
-                    if (monsters.Count == 0)
-                    {
-                        // we will generate a new set of monsters for a new round 
-                    }
+                {
+
+                }
+                else if (monsters.Count == 0)
+                {
+                    // we will generate a new set of monsters for a new round 
+                }
                 nextOneQueue.Dequeue();
 
-                // This method will be called upon beginning of the game, character movement, monster movement, character die, monster die events. 
 
 
             }
 
         }
 
+
+        //After every creature died, we will update our new battle map.
         public void UpdateGrid(Grid map, int[][] mapAry)
         {
 
@@ -88,7 +141,7 @@ namespace TheLastHero.Views
         public void CheckNextTurn()
         {
 
-            //dequeue
+            //dequeue when creature died
             var Creature = nextOneQueue.Dequeue();
 
             //Check whether the creature is still alive
@@ -104,10 +157,13 @@ namespace TheLastHero.Views
         }
 
 
-
+        // This method will generate monsters for each round. Initially, the game will have 
+        // six monsters and each four rounds, the method will generate a boss monster to       
+        // characters
         public List<Monster> GenerateMonsters(int curRound)
         {
 
+            //Check whether the monster is the boss monster
             bool isBoss = false;
 
             // After every three rounds, our game will generate a new boss monster
@@ -119,8 +175,8 @@ namespace TheLastHero.Views
             }
             else
             {
-                //Need to reconsider 
-                // discuss it later
+                // Generate normal monsters
+
                 for (int i = 0; i < 6; i++)
                 {
                     Monster monster = new Monster(curRound, isBoss);
@@ -133,3 +189,4 @@ namespace TheLastHero.Views
 
     }
 }
+
